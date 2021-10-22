bcd2d1c924828a196e315c6569148131074cbe09
genuinegod <50383375+genuinegod@users.noreply.github.com>
2021-10-23 02:01:01 +0900

    create momentum function

---
+++ momentum/js/todo.js
@@ -0,0 +1,55 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDo() {
+    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos));
+}
+
+function handleListDel(event) {
+    const li = event.target.parentElement;
+    li.remove();
+    toDos = toDos.filter((toDo) => toDo.id != parseInt(li.id));
+    saveToDo();
+}
+
+function paintToDo(newTodo) {
+    const li = document.createElement("li");
+    li.id = newTodo.id;
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click",handleListDel);
+    li.appendChild(span);
+    li.appendChild(button);
+    span.innerText = newTodo.text;
+    toDoList.appendChild(li);
+
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    const newTodoObj = {
+        text:newTodo,
+        id:Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
+    saveToDo();
+}
+
+toDoForm.addEventListener("submit",handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
